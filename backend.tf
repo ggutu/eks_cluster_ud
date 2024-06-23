c4669c641007735a634c9225fd7336e66b3d51d4
ggutu <151479796+ggutu@users.noreply.github.com>
2024-06-22 22:45:26 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bbb12-bucket"
+    bucket = "bbb12-bucketjune1045pm"
     key    = "infra_resources/terraform.tfstate"
     region = "us-east-2"
    # dynamodb_table = "terraform-state-locking"
